089f998018f7e880c425a222a549288df1856327
Chidsanu_ <Chidsanupongboonma@gmail.com>
2023-09-28 01:08:15 +0700

    test

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route('/hello/<name>', methods=['GET'])
 def hello(name):
-    return "Hello Test, " + str(name)
+    return "Hello, " + str(name)
 
 
 @app.route('/calculate/<num1>/<num2>', methods=['GET'])
